45034cc1a4456c4d6f14d503d3230522b00ed8e2
DineshTeja <51058492+DineshTeja@users.noreply.github.com>
2022-06-14 13:58:23 -0500

    l

--- WebApp/app.py
+++ WebApp/app.py
@@ -7,7 +7,7 @@ from PIL import Image
 
 
 # Custom imports 
-from multipage import MultiPage
+#from multipage import MultiPage
 #from pages import predict_page,explore_page,housepred
 
 from pages.predict_page import show_predict_page
@@ -15,7 +15,7 @@ from pages.explore_page import show_explore_page
 from pages.housepred import show_house_page
 
 # Create an instance of the app 
-app = MultiPage()
+#app = MultiPage()
 
 # Title of the main page
 image = Image.open('WebApp/images/polydatalogo.png')
@@ -35,10 +35,10 @@ with col3:
 
 
 # Add all your applications (pages) here
-app.add_page("U.S. Presidential Prediction Model", show_predict_page)
-app.add_page("U.S. Senate Prediction Model", show_explore_page)
-app.add_page("U.S. House Prediction Model", show_house_page)
+#app.add_page("U.S. Presidential Prediction Model", show_predict_page)
+#app.add_page("U.S. Senate Prediction Model", show_explore_page)
+#app.add_page("U.S. House Prediction Model", show_house_page)
 
 
 # The main app
-app.run()
+##app.run()
