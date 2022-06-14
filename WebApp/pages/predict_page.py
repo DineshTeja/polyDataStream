921796d62b86e17d16a98f1b3d1404d6c82136a8
DineshTeja <51058492+DineshTeja@users.noreply.github.com>
2022-06-14 14:06:56 -0500

    Update predict_page.py

--- WebApp/pages/predict_page.py
+++ WebApp/pages/predict_page.py
@@ -40,11 +40,6 @@ from PIL import Image
 
 def show_predict_page():
 
-    #titleImage = Image.open('images/usprestitle.png')
-
-    #t.image(titleImage,width=690)
-
-
     #title = st.title("U.S. Presidential Election Prediction Model (1976 - 2020)")
     results = st.container()
     actualResults = st.expander("View the actual election results")
