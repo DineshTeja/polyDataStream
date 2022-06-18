9df2b18aa2c9c7126d19505357c4c8ea89f6e895
DineshTeja <51058492+DineshTeja@users.noreply.github.com>
2022-06-18 13:27:28 -0500

    Update app.py

--- WebApp/app.py
+++ WebApp/app.py
@@ -58,11 +58,11 @@ with col1:
 with col2:
     image3 = Image.open('WebApp/images/mitElection.png')
             
-    st.image(image3,width=360)
+    st.image(image3,width=310)
 
     image4 = Image.open('WebApp/images/arkdems.png')
             
-    st.image(image4,width=300)
+    st.image(image4,width=290)
 
 
 
